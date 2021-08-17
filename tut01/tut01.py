3a75c59d7e255ec51db2319d81d114d3b60de239
Prasanth Kanakala <prasanthkanakala75@gmail.com>
2021-08-17 20:58:58 +0530

    Update tut01.py

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -5,14 +5,12 @@ print("My name is Kanakala Durga Prasanth. My Roll no is 1901EE31")
 def meraki_helper(n):
     """This will detect meraki number"""
     if(n<10):
-	#case for numbers less than 10
         print("Yes-{} is a meraki number".format(n))
         return 1
-    a=str(n)  #converting int value to a string
+    a=str(n)  
     i=0
     while(i<len(a)-1):
         if(abs(int(a[i])-int(a[i+1]))==1):
-            #condition for checking whether the number is meraki...
 	    i+=1
         else:
             print("No-{} is not a meraki number".format(n))
@@ -24,7 +22,7 @@ def meraki_helper(n):
 
 
 input = [12, 14, 56, 78, 98, 54, 678, 134, 789, 0, 7, 5, 123, 45, 76345, 987654321]
-count1=0    #counter variable to keep the record of numbers
+count1=0    
 count2=0
 for i in input:
     if(meraki_helper(i)):
